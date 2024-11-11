f7b81c95cb6b05d82e575526e33f29ac40a35377
Kevin Wong <kevinwong973@gmail.com>
2024-11-11 15:52:32 -0800

    connected Stripe to database and user checkout page, fixed bug where name email address and city were not included in the api/checkout POST, also integrated Stripe with checkout page, added an Order schema as well

--- pages/checkout.js
+++ pages/checkout.js
@@ -44,77 +44,84 @@ export default function CheckoutPage() {
     <Layout>
       {!productsInfos.length && <div>no products in your shopping cart</div>}
       {productsInfos.length &&
-        productsInfos.map((productInfo) => (
-          <div className="flex mb-5" key={productInfo._id}>
-            <div className="bg-gray-100 p-3 rounded-xl shrink-0">
-              <img className="w-24" src={productInfo.picture} alt="" />
-            </div>
-            <div className="pl-4">
-              <h3 className="font-bold text-lg">{productInfo.name}</h3>
-              <p className="text-sm leading-4 text-gray-500">
-                {productInfo.description}
-              </p>
-              <div className="flex">
-                <div className="grow">${productInfo.price}</div>
-                <div>
-                  <button
-                    onClick={() => lessOfThisProduct(productInfo._id)}
-                    className="border border-emerald-500 px-2 rounded-lg text-emerald-500"
-                  >
-                    -
-                  </button>
-                  <span className="px-2">
-                    {
-                      selectedProducts.filter((id) => id === productInfo._id)
-                        .length
-                    }
-                  </span>
-                  <button
-                    onClick={() => moreOfThisProduct(productInfo._id)}
-                    className="bg-emerald-500 px-2 rounded-lg text-white"
-                  >
-                    +
-                  </button>
+        productsInfos.map((productInfo) => {
+          const amount = selectedProducts.filter(
+            (id) => id === productInfo._id
+          ).length;
+          if (amount === 0) return;
+          return (
+            <div className="flex mb-5" key={productInfo._id}>
+              <div className="bg-gray-100 p-3 rounded-xl shrink-0">
+                <img className="w-24" src={productInfo.picture} alt="" />
+              </div>
+              <div className="pl-4">
+                <h3 className="font-bold text-lg">{productInfo.name}</h3>
+                <p className="text-sm leading-4 text-gray-500">
+                  {productInfo.description}
+                </p>
+                <div className="flex">
+                  <div className="grow">${productInfo.price}</div>
+                  <div>
+                    <button
+                      onClick={() => lessOfThisProduct(productInfo._id)}
+                      className="border border-emerald-500 px-2 rounded-lg text-emerald-500"
+                    >
+                      -
+                    </button>
+                    <span className="px-2">
+                      {
+                        selectedProducts.filter((id) => id === productInfo._id)
+                          .length
+                      }
+                    </span>
+                    <button
+                      onClick={() => moreOfThisProduct(productInfo._id)}
+                      className="bg-emerald-500 px-2 rounded-lg text-white"
+                    >
+                      +
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      <div className="mt-4">
-        <input
-          name="address"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
-          type="text"
-          placeholder="Street address, number"
-        />
-        <input
-          name="city"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
-          type="text"
-          placeholder="City and postal code"
-        />
-        <input
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
-          type="text"
-          placeholder="Your name"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
-          type="email"
-          placeholder="Email address"
-        />
-      </div>
+          );
+        })}
       <form action="/api/checkout" method="POST">
+        <div className="mt-8">
+          <input
+            name="address"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
+            type="text"
+            placeholder="Street address, number"
+          />
+          <input
+            name="city"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
+            type="text"
+            placeholder="City and postal code"
+          />
+          <input
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
+            type="text"
+            placeholder="Your name"
+          />
+          <input
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="bg-gray-100 w-full rounded-lg px-4 py-2 mb-2"
+            type="email"
+            placeholder="Email address"
+          />
+        </div>
+
         <div className="mt-4">
           <div className="flex my-3 ">
             <h3 className="grow font-bold text-gray-400">Subtotal:</h3>
