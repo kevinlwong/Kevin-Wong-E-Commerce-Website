09290da035c8f65f73adfea1a8d4ee8d5d646386
Kevin Wong <kevinwong973@gmail.com>
2024-11-11 17:54:57 -0800

    reverted from pages to using vercel for hosting

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,5 @@
 const nextConfig = {};
 
 
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  output: 'export', // Enable static export
-  basePath: isProd ? '/Kevin-Wong-E-Commerce-Website' : '', // Replace <your-repo-name> with your GitHub repo name
-  images: {
-    unoptimized: true, // Disable image optimization for static sites
-  },
-};
-
 
 export default nextConfig;
